cd4042409971de314bd9fdf0f8839211ffe7b3bd
Tom <fake.address@gmail.com>
2014-03-16 18:11:31 -0700

    Adds polygon abstract class

--- code.js
+++ code.js
@@ -18,7 +18,49 @@ var ltime;		// Keep track of time between current and last tick
 var tempx=100;		// temp x&y vars of moving box
 var tempy=100;
 
+// A Javascript object intended for use as an abstract class
+var polygon = {
+    xcoords: undefined,
+    ycoords: undefined,
+    color: undefined,
+    draw: function() {
+        if (typeof this.xcoords === 'undefined'
+            || typeof this.ycoords === 'undefined'
+            || typeof this.color === 'undefined'
+            || this.xcoords.length < 3
+            || this.ycoords.length != this.xcoords.length) {
+            // TODO: Find a better way to alert us to these types of errors
+            alert("Programming error: we tried to draw an improperly constructed polygon");
+        }
 
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.moveTo(this.xcoords[0], this.ycoords[0]);
+        for (var i = 0; i < this.xcoords.length; i ++) {
+            ctx.lineTo(this.xcoords[i], this.ycoords[i]);
+        }
+        ctx.closePath();
+        ctx.fill();
+    },
+    moveX: function(distance) {
+        for (var i = 0; i < this.xcoords.length; i ++) {
+            this.xcoords[i] = this.xcoords[i] + distance;
+        }
+    },
+    moveY: function(distance) {
+        for (var i = 0; i < this.ycoords.length; i ++) {
+            this.ycoords[i] = this.ycoords[i] + distance;
+        }
+    }
+};
+
+var raptor = Object.create(polygon);
+
+function initializeRaptor() {
+    raptor.color = "#FF0000";
+    raptor.xcoords = [60, 80, 100];
+    raptor.ycoords = [100, 50, 100];
+}
 
 // Init routine
 $(document).ready(function()
@@ -41,28 +83,25 @@ $(document).ready(function()
 	{
 		keydown[e.keyCode] = false;
 	});
-	
+
+    initializeRaptor();
 
 	// Begin endless delayed loop
 	tick();
-
 });
 
 
 // This function is executed once every 30 ms
 function tick()
 {
-    updateRaptor();
+    // Process key commands, mouse movements, etc.
+    processUserInput();
 
 	// Clear the canvas
 	ctx.clearRect(0,0,width,height);
 
-	// Draw the box, with the topleft corner at coordinates (tempx,tempy)
-	ctx.fillStyle="#000000";
-	ctx.fillRect(tempx,tempy,20,20);
-
-
-
+    // TODO: Consider replacing this with something more general for all drawable objects.
+    raptor.draw();
 
 	// Calculate time left until next tick, set timer
 	var time = (new Date()).getTime();
@@ -71,26 +110,25 @@ function tick()
 	setTimeout(tick,30-dtime);
 }
 
-function updateRaptor() {
-    // Alter tempx/tempy if certain keys pressed
+function processUserInput() {
     if(keydown[rightkey])
     {
-        tempx += 1;
+        raptor.moveX(1);
     }
 
     if(keydown[leftkey])
     {
-        tempx -= 1;
+        raptor.moveX(-1);
     }
 
     if(keydown[upkey])
     {
-        tempy -= 1;
+        raptor.moveY(-1);
     }
 
     if(keydown[downkey])
     {
-        tempy +=1;
+        raptor.moveY(1);
     }
 }
 
